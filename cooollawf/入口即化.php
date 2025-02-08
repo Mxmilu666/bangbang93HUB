697515899e4b7b968b344da9b315c5bd4ff3e0aa
weyeah <121325752+weyeahh@users.noreply.github.com>
2025-02-08 11:08:28 +0800

    入口即化.png

---
+++ cooollawf/入口即化.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="zh"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>MCBBS 神教</title><meta name="description" content=""><meta name="viewport" content="width=device-width, initial-scale=1.0"><script>window.va = window.va || function(){(window.vaq = window.vaq || []).push(arguments)};</script><style>h1{font-weight:500;color:#4caf50;margin-bottom:-16px}h2{opacity:.8}h2,h3,h4{font-weight:300;line-height:2}a[href]{color:#4caf50;text-decoration:none}a[href]:hover{opacity:.8;text-decoration:underline}hr{border:0;border-top:black 1px solid;opacity:.3}html{max-width:800px;margin:auto;background:#fafafa;font-family:Candara,Helvetica,Microsoft Yahei,sans-serif}@media(prefers-color-scheme:dark){html{background:#1f1f1f;color:#f0f0f0}code{background:#3c3c3c}hr{border-color:#fafafa}}</style></head><body><h1>回来吧 MCBBS</h1><h2>千万人的信仰</h2><h3>眼前景象，忧伤难掩。MCBBS，昔日众人栖息之所，今已销声匿迹。<br>忆起当年，此地熙熙攘攘，生机勃勃，今则只余一片寂静。<br>自二零一零年诞生以来，乃无数MC玩家之心灵家园。<br>于此，可尽情创造、探索、交流，分享喜怒哀乐。<br>每一篇帖子，每一番回复，皆我心意之交融，皆我游戏生涯之一部分。<br>昔日，我于MCBBS结识诸多志同道合之友。<br>共商游戏技艺，分享建造心得，乃至游戏之外，亦成知己。<br>于此虚拟之地，寻得一片属于己者之天地，彼此相伴、相扶、相励。<br>然，现实乃为残酷。岁月流逝，MCBBS渐陷沉寂。<br>闭站之讯传来，初以为暂时之停顿，然现实却残酷告诉，MCBBS已逝。<br>往日熟悉之景，往日热络之境，今随风而逝，唯余一抹忧伤。<br>我永怀念曾伴我成长之MCBBS，怀念与我共游戏之友。<br>虽闭站，然于我记忆中永存。<br>愿其于虚拟之境安息，愿我永怀其美好。<br>MCBBS，再会，愿尔于游戏之天堂永自由飞扬。<br></h3><hr><h2>回来吧！ MCBBS！</h2><center>2024 © mcbbs.tax<br>云望IT | <br>由 MCWizrd Team 驱动</center><br><center><a href="https://mcbbs.xin/link">简幻欢短链生成</a></center><br><br><br><br><br><br><br><br><br><video src="https://vdse.bdstatic.com//192d9a98d782d9c74c96f09db9378d93.mp4" controls="" id="rick" __idm_id__="4898817"></video><h1>你被骗了</h1><br>
+<script>
+var video = document.getElementById('rick');
+document.addEventListener('DOMContentLoaded', function() {
+  video.play().catch(function(error) {
+    document.addEventListener('click', function() {
+      video.play();
+    });
+  });
+});
+</script>
+
+<div id="synofoto-browser-extension-loaded"></div><input type="hidden" value="chrome_extension" data-id="abcdefghijk" name="chrome_extension"></body></html>
